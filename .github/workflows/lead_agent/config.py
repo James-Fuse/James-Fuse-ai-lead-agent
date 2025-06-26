3ce17f003d9c0fa4cf2b71e0f7d91d331cb0ff97
James-Fuse <Justin_wehrheim@yahoo.de>
2025-06-26 11:45:07 +0200

    Create config.py

---
+++ .github/workflows/lead_agent/config.py
@@ -0,0 +1,29 @@
+# lead_agent/config.py
+
+# E-Mail-Empfänger
+EMAIL_TO = "info@james-fuse.de"
+
+# SMTP Einstellungen (Gmail Beispiel)
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USERNAME = "mj.mix888@gmail.com"
+SMTP_PASSWORD = "dndg cizt plii mvtm"  # App-spezifisches Passwort
+
+# Begriffe für Suchagenten
+SEARCH_TERMS = [
+    "Bussmann Sicherung",
+    "FNQ-R",
+    "LP-CC",
+    "KTK",
+    "Industriesicherung",
+    "UL Sicherung",
+    "Class CC Sicherung",
+    "Sicherungskauf"
+]
+
+# TED / bund / eBay etc.
+TED_URL = "https://ted.europa.eu/TED/search/search.do"
+BUND_URL = "https://www.bund.de/"
+
+# eBay Beispielkategorie für Elektronik
+EBAY_KEYWORDS = ["Bussmann", "UL Sicherung", "FNQ-R", "LP-CC"]
