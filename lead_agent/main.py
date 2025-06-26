6a20d8a7d5918189e746e8e48e3fd2f86da59de6
James-Fuse <Justin_wehrheim@yahoo.de>
2025-06-26 12:53:29 +0200

    Update main.py

--- lead_agent/main.py
+++ lead_agent/main.py
@@ -1,8 +1,8 @@
-from lead_agent.ted_scraper import search_ted
-from lead_agent.bund_scraper import search_bund
-from lead_agent.kleinanzeigen_checker import search_kleinanzeigen
-from lead_agent.website_crawler import search_industrystock
-from lead_agent.email_reporter import send_email
+from ted_scraper import search_ted
+from bund_scraper import search_bund
+from kleinanzeigen_checker import search_kleinanzeigen
+from website_crawler import search_industrystock
+from email_reporter import send_email
 
 def main():
     ted_results = search_ted()
